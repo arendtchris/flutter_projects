32f3a656ab668a9c324533f569ef2a61593f488f
carendt <Christian.Arendt@itk-engineering.de>
2023-07-25 17:30:14 +0200

    initial upload flutter location app

---
+++ flutter_application_ios/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <geolocator_windows/geolocator_windows.h>
+#include <win_ble/win_ble_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
+  WinBlePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WinBlePlugin"));
+}
